526d42a57de987d238b83dac1525108f2f51c609
ajinkyabh2203 <ajinkyabh2203@gmail.com>
2024-07-11 13:05:51 +0530

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+  {
+    rules: {
+      "import/extensions": 0,
+      "react/prop-types": 0,
+      "linebreak-style": 0,
+      "react/state-in-constructor": 0,
+      "import/prefer-default-export": 0,
+      "max-len": [2, 250],
+      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 1 }],
+      "no-underscore-dangle": [
+        "error",
+        { allow: ["_d", "_dh", "_h", "_id", "_m", "_n", "_t", "_text"] },
+      ],
+      "object-curly-newline": 0,
+      "react/jsx-filename-extension": 0,
+      "react/jsx-one-expression-per-line": 0,
+      "jsx-a11y/click-events-have-key-events": 0,
+      "jsx-a11y/alt-text": 0,
+      "jsx-a11y/no-autofocus": 0,
+      "jsx-a11y/no-static-element-interactions": 0,
+      "react/no-array-index-key": 0,
+      "no-param-reassign": 0,
+      "react/react-in-jsx-scope": 0,
+      "jsx-a11y/anchor-is-valid": [
+        "error",
+        {
+          components: ["Link"],
+          specialLink: ["to", "hrefLeft", "hrefRight"],
+          aspects: ["noHref", "invalidHref", "preferButton"],
+        },
+      ],
+    },
+  },
+];
