15d326826388962d48881a5377a2b41942ab7018
ajinkyabh2203 <ajinkyabh2203@gmail.com>
2024-07-14 18:00:49 +0530

    Addes movie posters, ratings and links

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { Grow, Tooltip, Rating } from "@mui/material";
-import { Link } from "react-router-dom";
 import { styles } from "./styles";
 const Movie = ({ movie, i }) => {
-  const { Grid, Typography } = styles;
   console.log(movie, i);
+  const { Grid, Typography, Link, Image } = styles;
 
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
-      <Typography variant="h5">{movie.title}</Typography>
+      <Grow in key={i} timeout={(i + 1) * 250}>
+        <Link to={`/movie/${movie.id}`}>
+          <Image
+            alt={movie.title}
+            src={
+              movie.poster_path
+                ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                : "https://www.fillmurray.com/200/300"
+            }
+          />
+          <Typography variant="h5">{movie.title}</Typography>
+          <Tooltip disableTouchListener title={`${movie.vote_average}/10` }>
+            <div>
+              <Rating readOnly value={movie.vote_average / 2} precision={0.1} />
+            </div>
+          </Tooltip>
+        </Link>
+      </Grow>
     </Grid>
   );
 };
--- src/components/Movie/styles.js
+++ src/components/Movie/styles.js
@@ -1,8 +1,32 @@
 import { styled } from "@mui/material/styles";
+import { Link as domlink } from "react-router-dom";
 import { Grid as muiGrid, Typography as muiTypo } from "@mui/material";
+import { display } from "@mui/system";
 
 const Grid = styled(muiGrid)({
   padding: "10px",
+});
+
+const Link = styled(domlink)(({ theme }) => ({
+  alignItems: "center",
+  fontWeight: "bolder",
+  textDecoration: "none",
+  [theme.breakpoints.up("xs")]: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  "&:hover": {
+    cursor: "pointer",
+  },
+}));
+
+const Image = styled("img")({
+  borderRadius: "20px",
+  height: "300px",
+  marginBottom: "10px",
+  "&:hover": {
+    transform: "scale(1.05)",
+  },
 });
 
 const Typography = styled(muiTypo)(({ theme }) => ({
@@ -19,4 +43,6 @@ const Typography = styled(muiTypo)(({ theme }) => ({
 export const styles = {
   Grid,
   Typography,
+  Link,
+  Image,
 };
